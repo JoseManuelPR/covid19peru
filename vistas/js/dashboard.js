6f20a4ca30262f663040af78f3b0d4859460c351
josema <git@devlab.com>
2020-04-06 13:35:18 -0500

    cargando proyecto al 06/4 a las 1:34 PM

--- vistas/js/dashboard.js
+++ vistas/js/dashboard.js
@@ -537,7 +537,7 @@ const getCities = async () => {
 
 getCities();
 
-function initMap() {
+async function initMap() {
     let infowindow = new google.maps.InfoWindow(); // Only one InfoWindow
     // Create the map.
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -641,7 +641,7 @@ function initMap() {
                 fillOpacity: 0.25,
                 map: map,
                 center: citymap[city].center,
-                radius: Math.sqrt(citymap[city].population) * 7500
+                radius: Math.sqrt(citymap[city].population) * 6000
             });
     
             cityCircle.addListener('click', function(ev){
@@ -653,5 +653,117 @@ function initMap() {
         }
     }
 
-    
+    const BASE_API = './data/zoneCases.json'
+    const getData = async (url) => {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+        throw new Error('No se encontró ningun resultado');
+    }
+
+    try {
+        const locations = await getData(`${BASE_API}`)
+
+        var mapZones = new google.maps.Map(document.getElementById('mapZones'), {
+            zoom: 5,
+            center: {lat: -10.1899672, lng: -75.015152},
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            disableDefaultUI: true,
+            styles: [
+                {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+                {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+                {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+                {
+                    featureType: 'administrative.locality',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#d59563'}]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#d59563'}]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'geometry',
+                    stylers: [{color: '#263c3f'}]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#6b9a76'}]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{color: '#38414e'}]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry.stroke',
+                    stylers: [{color: '#212a37'}]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#9ca5b3'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{color: '#746855'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry.stroke',
+                    stylers: [{color: '#1f2835'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#f3d19c'}]
+                },
+                {
+                    featureType: 'transit',
+                    elementType: 'geometry',
+                    stylers: [{color: '#2f3948'}]
+                },
+                {
+                    featureType: 'transit.station',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#d59563'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'geometry',
+                    stylers: [{color: '#17263c'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#515c6d'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.stroke',
+                    stylers: [{color: '#17263c'}]
+                }
+            ]
+        });
+
+        // Create an array of alphabetical characters used to label the markers.
+
+        var markers = locations.map(function(location, i) {
+            return new google.maps.Marker({
+                position: location
+            });
+        });
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(mapZones, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
+    } catch (error) {
+        alert(error.message)
+    }
 }
